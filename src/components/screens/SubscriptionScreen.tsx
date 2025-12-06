fb8d61cac96e6fef869e89f64743d84e5771ab3b
Your Name <you@example.com>
2025-12-06 18:43:13 +0530

    Update free plan text to be more generic - replace 'practice a case everyday' with 'access to core features'

--- src/components/screens/SubscriptionScreen.tsx
+++ src/components/screens/SubscriptionScreen.tsx
@@ -94,7 +94,7 @@ export const SubscriptionScreen: React.FC = () => {
                 <svg className="w-5 h-5 text-green-500 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
                 </svg>
-                <span>Practice a case everyday</span>
+                <span>Access to core features</span>
               </li>
             </ul>
             <button
